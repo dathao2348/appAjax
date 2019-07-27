bb9808588bb6e3c6ed3af912324b4dc33aa3109b
admin <tranthidathao234@gmail.com>
2019-07-27 16:26:17 +0700

    234

---
+++ jsonphp/index2.php
@@ -0,0 +1,14 @@
+<?php
+$json1='["Ha Noi","Ho Chi Minh","Da Nang"]';
+$json2='{"name":"Nguyen Van An","age":21,"location":"Ha Noi"}';
+
+$convert1=json_decode($json1);
+$convert2=json_decode($json2);
+
+echo "<pre>";
+print_r($convert1);
+echo "</pre>";
+
+echo "<pre>";
+print_r($convert2);
+echo "</pre>";
